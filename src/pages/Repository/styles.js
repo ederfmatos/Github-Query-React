795797c5c19c21cbf752a22ed007463fbf4f930a
ederfmatos <ederfmatos@gmail.com>
2019-12-29 11:57:07 -0300

    Listando repositorios

--- src/pages/Repository/styles.js
+++ src/pages/Repository/styles.js
@@ -40,3 +40,64 @@ export const Owner = styled.div`
     text-decoration: none;
   }
 `;
+
+export const IssueList = styled.ul`
+  padding-top: 30px;
+  margin-top: 30px;
+  border-top: 1px solid #ddd;
+  list-style: none;
+
+  li {
+    display: flex;
+    padding: 15px 10px;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+
+    & + li {
+      margin-top: 10px;
+    }
+
+    img {
+      width: 36px;
+      height: 36px;
+      border-radius: 50px;
+      border: 2px solid #ddd;
+    }
+
+    div {
+      flex: 1;
+      margin-left: 15px;
+
+      strong {
+        font-size: 16px;
+
+        a {
+          text-decoration: none;
+          color: #333;
+
+          &:hover {
+            color: #2a8642;
+          }
+        }
+
+        span {
+          background: #eee;
+          color: #333;
+          border-radius: 2px;
+          font-size: 12px;
+          font-weight: 600px;
+          height: 20px;
+          padding: 3px 4px;
+          margin-left: 10px;
+        }
+      }
+    }
+
+    p {
+      margin-top: 5px;
+      font-size: 12px;
+
+      color: #999;
+    }
+  }
+`;
